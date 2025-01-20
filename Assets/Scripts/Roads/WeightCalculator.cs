ae24aaef2135b28943ce3850824e8fb21b2e3f1d
marwan <aitaddi.marwan@gmail.com>
2025-01-20 11:58:01 +0100

    Squelette weight Calculator

---
+++ Assets/Scripts/Roads/WeightCalculator.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Probably refactor those to include them in the Graph class
+ *
+ * Source : https://onlinelibrary.wiley.com/doi/full/10.1111/j.1467-8659.2009.01612.x
+ */
+
+using System.Drawing;
+using UnityEngine.UIElements;
+
+namespace Roads
+{
+    public static class WeightCalculator
+    {
+        /*
+         * Characteristic functions
+         */
+
+        // Calculate slope between two points, starting from p1 to p2
+        // So if p1 is above p2 slope is negative and vice versa
+        public static float Slope(Position p1, Position p2)
+        {
+            return 0f;
+        }
+
+        // Calculate the angle formed by p1, p2 and p3
+        public static float Curvature(Position p1, Position p2, Position p3)
+        {
+            return 0f;
+        }
+        
+        /*
+         * Transfer functions
+         */
+        
+        public static float WhTransfer(float wh)
+        {
+            return 0f;
+        }
+
+        public static float STransfer(float s)
+        {
+            return 0f;
+        }
+
+        public static float CTransfer(float c)
+        {
+            return 0f;
+        }
+        
+        /*
+         * Weight function
+         * TODO: Need to have special weight functions or the first two points
+         */
+
+        // Weight of p3 depending on the two previous points
+        
+        public static float Weight(Position p1)
+        {
+            return 0f;
+        }
+        
+        public static float Weight(Position p1, Position p2)
+        {
+            return 0f;
+        }
+        
+        public static float Weight(Position p1, Position p2, Position p3)
+        {
+            return 0f;
+        }
+    }
+}
